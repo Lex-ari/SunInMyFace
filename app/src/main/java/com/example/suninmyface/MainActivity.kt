c5280464d4af64a3597c8c46e95e118626ab1e48
Alex Mariano <amarianocello@gmail.com>
2023-01-21 22:25:04 -0800

    Added Updating Clock

--- app/src/main/java/com/example/suninmyface/MainActivity.kt
+++ app/src/main/java/com/example/suninmyface/MainActivity.kt
@@ -4,6 +4,10 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.suninmyface.databinding.ActivityMainBinding
+import java.sql.Time
+import java.text.SimpleDateFormat
+import java.time.Clock
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +28,24 @@ class MainActivity : AppCompatActivity() {
                 binding.layoutMain.setBackgroundColor(Color.rgb(255, 0, 0))
             }
             isRed = !isRed
+        }
+
+        //https://stackoverflow.com/questions/55823042/how-to-update-this-textview-with-the-date-every-second
+        val timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask(){
+            override fun run(){
+                updateClock()
+            }
+        },0,1000)
+
+
+
+
+    }
+
+    private fun updateClock(){
+        runOnUiThread{
+            binding.textViewMainTrueTime.text = "${SimpleDateFormat("hh:mm:ss").format(Date())}"
         }
     }
 }
